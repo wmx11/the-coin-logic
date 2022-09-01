08903d19c818d33c449dc68846a0be88511fc10c
Vainius <vmykolaitis1@gmail.com>
2022-09-01 20:03:39 +0300

    Switched to env

--- packages/graphql/request.ts
+++ packages/graphql/request.ts
@@ -3,8 +3,7 @@ import axios from 'axios';
 const request = async <T>(query: string, variables?: T) => {
   try {
     const data = await axios({
-      url: 'http://localhost:3500/api/graphql',
-      // url: process.env.GRAPHQL_ENDPOINT,
+      url: process.env.GRAPHQL_ENDPOINT,
       method: 'POST',
       data: {
         query,
