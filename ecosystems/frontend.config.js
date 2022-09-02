cde0fcbd8b939cc17d306aa4a7bb46b29420a8f3
Vainius <vmykolaitis1@gmail.com>
2022-09-02 10:22:46 +0300

    More eco tweaks

--- ecosystems/frontend.config.js
+++ ecosystems/frontend.config.js
@@ -4,11 +4,15 @@ module.exports = {
     {
       name: 'admin',
       script: 'npm',
+      automation: false,
+      interpreter: 'none',
       args: 'run admin:start',
     },
     {
       name: 'client',
       script: 'npm',
+      automation: false,
+      interpreter: 'none',
       args: 'run client:start',
     },
   ],
