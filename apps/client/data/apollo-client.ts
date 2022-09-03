bc8232639e86b6577ed815653e54f1ab49e765ed
Vainius <vmykolaitis1@gmail.com>
2022-09-03 16:37:03 +0300

    Updated apollo client cache

--- apps/client/data/apollo-client.ts
+++ apps/client/data/apollo-client.ts
@@ -1,8 +1,13 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient } from '@apollo/client';
+import { InvalidationPolicyCache } from '@nerdwallet/apollo-cache-policies';
 
 const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
-  cache: new InMemoryCache()
+  cache: new InvalidationPolicyCache({
+    invalidationPolicies: {
+      timeToLive: 1000 * 60 * 5,
+    },
+  }),
 });
 
 export default client;
