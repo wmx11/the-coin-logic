a1fe540b101342157a68a4170e7b685d03fc4572
Vainius <vmykolaitis1@gmail.com>
2022-09-04 13:47:33 +0300

    Track transfer events every 10 minutes

--- apps/holders-tracker/jobs/trackTransferEventsAndHoldings.ts
+++ apps/holders-tracker/jobs/trackTransferEventsAndHoldings.ts
@@ -1,5 +1,5 @@
 import cron from 'node-cron';
 import trackTransferEventsAndHoldings from '../services/trackTransferEventsAndHoldings';
 
-// Run every 5 minutes to get transfer events and create/update wallet balances
-cron.schedule('*/5 * * * *', () => trackTransferEventsAndHoldings());
+// Run every 10 minutes to get transfer events and create/update wallet balances
+cron.schedule('*/10 * * * *', () => trackTransferEventsAndHoldings());
