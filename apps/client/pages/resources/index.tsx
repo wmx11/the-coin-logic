33da439df762175ddb96bf292d42a6bcb16bfdca
Vainius <vmykolaitis1@gmail.com>
2022-09-12 13:15:08 +0300

    Tweaked some individual pages

--- apps/client/pages/resources/index.tsx
+++ apps/client/pages/resources/index.tsx
@@ -1,9 +1,10 @@
-import { Button, Title } from '@mantine/core';
+import { Button, Container, Text, Title } from '@mantine/core';
 import SmallHero from 'components/SmallHero';
-import TextContent from 'components/TextContent';
-import TextContentWrapper from 'components/TextContent/TextContentWrapper';
-import { getContentBySlug } from 'data/getters';
+import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
+import CardinalHouse from 'public/images/cardinal_house.jpg';
+import KCConsulting from 'public/images/kc_consulting.jpg';
 import { FC } from 'react';
 import { ContentProps } from 'types/TextContent';
 
@@ -23,21 +24,50 @@ const index: FC<ContentProps> = ({ content }) => {
           Contact Us
         </Button>
       </SmallHero>
-      <TextContentWrapper content={content}>
-        <TextContent className="m-auto max-w-none" />
-      </TextContentWrapper>
+
+      <Container className="py-10">
+        <Title order={1} className="text-5xl mb-4" align="center">
+          Our Partners
+        </Title>
+        <Text size="sm" color="dimmed" align="center" className="mb-16">
+          Read more about our partners and how they could help your project succeed!
+        </Text>
+
+        <div className="flex gap-16 items-center justify-center flex-wrap">
+          <div className="max-w-[120px] w-full rounded-md relative group p-2 flex items-center">
+            <Image src={CardinalHouse} />
+            <Link href="/text-content/cardinal-house">
+              <a>
+                <div className="bg-violet text-white absolute inset-0 justify-center items-center flex opacity-0 group-hover:opacity-90 transition-all rounded-md shadow-md">
+                  Read more
+                </div>
+              </a>
+            </Link>
+          </div>
+          <div className="max-w-[200px] w-full rounded-md relative group p-2 flex items-center">
+            <Image src={KCConsulting} />
+            <Link href="/text-content/kc-consulting">
+              <a>
+                <div className="bg-violet text-white absolute inset-0 justify-center items-center flex opacity-0 group-hover:opacity-90 transition-all rounded-md shadow-md">
+                  Read more
+                </div>
+              </a>
+            </Link>
+          </div>
+          <div className="rounded-md relative group p-2 flex items-center">
+            <Title order={1}>schedule.me</Title>
+            <Link href="/text-content/schedule-me">
+              <a>
+                <div className="bg-violet text-white absolute inset-0 justify-center items-center flex opacity-0 group-hover:opacity-90 transition-all rounded-md shadow-md">
+                  Read more
+                </div>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </Container>
     </>
   );
 };
 
 export default index;
-
-export const getServerSideProps = async () => {
-  const content = await getContentBySlug('resources');
-
-  return {
-    props: {
-      content,
-    },
-  };
-};
--- apps/client/pages/services/index.tsx
+++ apps/client/pages/services/index.tsx
@@ -1,17 +1,19 @@
-import { Button, Container, Text, Title } from '@mantine/core';
+import { Button, Container, ScrollArea, Text, Title } from '@mantine/core';
+import { useResizeObserver } from '@mantine/hooks';
 import Meta from 'components/Meta';
 import SmallHero from 'components/SmallHero';
 import TextContent from 'components/TextContent';
 import { getContentBySlug } from 'data/getters';
-import Image, { StaticImageData } from 'next/image';
+import { StaticImageData } from 'next/image';
 import { useRouter } from 'next/router';
-import auditing from 'public/images/auditing.png';
 import { FC, ReactNode } from 'react';
-import { AiOutlineBarChart } from 'react-icons/ai';
-import { FiTarget, FiUserCheck } from 'react-icons/fi';
+import { AiOutlineSecurityScan } from 'react-icons/ai';
+import { FiTarget } from 'react-icons/fi';
 import { GiNestBirds } from 'react-icons/gi';
 import { IoMicSharp } from 'react-icons/io5';
-import { MdEmojiEvents } from 'react-icons/md';
+import { MdBarChart, MdEmojiEvents } from 'react-icons/md';
+import { RiShieldUserLine } from 'react-icons/ri';
+import { animated, useSpring } from 'react-spring';
 import { ContentProps } from 'types/TextContent';
 
 type ServiceCardProps = {
@@ -23,19 +25,33 @@ type ServiceCardProps = {
 };
 
 const ServiceCard: FC<ServiceCardProps> = ({ title, description, image, alt, icon }) => {
+  const [ref, rect] = useResizeObserver();
+  const styles = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
+
   return (
-    <div className="flex items-stretch p-10">
-      <div className="flex-1">
-        <Title order={2} className="text-violet mb-2 flex justify-between items-end">
-          <div className="flex-1">{title}</div>
-          <div className="flex-0 flex justify-end ml-5">
-            {image && <Image src={image} alt={alt as string} width="80px" height="80px" layout="intrinsic" />}
-            {icon && icon}
+    <>
+      <animated.div
+        className="p-4 shadow-md rounded-md border group bg-violet hover:shadow-lg hover:scale-105 transition-transform cursor-pointer relative overflow-hidden"
+        ref={ref}
+        style={{ ...styles }}
+      >
+        <div className="relative w-full h-full flex items-center justify-center z-10">
+          <Title
+            className="text-white p-10 group-hover:opacity-0 transition-opacity text-center flex flex-col justify-center items-center"
+            order={1}
+            align="center"
+          >
+            {title}
+          </Title>
+          <div className="hidden text-white group-hover:block absolute left-0 right-0 top-0 h-full">
+            <ScrollArea style={{ height: rect.height - 16 || 160 }}>
+              <Text size="sm">{description}</Text>
+            </ScrollArea>
           </div>
-        </Title>
-        <Text size="sm">{description}</Text>
-      </div>
-    </div>
+          <div className="absolute right-0 top-0 text-white group-hover:opacity-0 transition-opacity">{icon}</div>
+        </div>
+      </animated.div>
+    </>
   );
 };
 
@@ -57,42 +73,42 @@ const index: FC<ContentProps> = ({ content }) => {
         </Button>
       </SmallHero>
       <Container className="py-10">
-        <div className="grid md:grid-cols-2 gap-8">
+        <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-8">
           <ServiceCard
             title="Listing & Tracking"
             description="List and track your project data in real-time. Bring transparency through data right to your community! General market data such as price, market cap, supply, liquidity, custom data like treasury, insurance, and utility wallets. Track holders from a specified token amount. Never be out of the loop with what’s happening with your project."
-            icon={<AiOutlineBarChart size={80} className="text-violet" />}
+            icon={<MdBarChart size={35} />}
           />
           <ServiceCard
             title="Contract Audits"
             description="Smart contract audits are one of the best ways to ensure your project is secure and free of logic errors. Audits can help you spot bugs way before the project grows to sizes where migrations are a headache to handle. Audit your project smart contract with one of the most competitive prices in the space through our partners at Cardinal House! They are revolutionizing the space in a couple key ways in addition to all audit standards. Firstly, Cardinal House Audits do a manual review of the smart contracts to ensure that all intended functionalities are present. Secondly, they also do Unit/Integration Testing as a secondary service to dive deeper into all the edge cases."
-            image={auditing}
+            icon={<AiOutlineSecurityScan size={35} />}
             alt="Auditing logo"
           />
           <ServiceCard
-            title="K.Y.C (Know Your Customer)"
+            title="K.Y.C"
             description="K.Y.C is one way to make you and your project transparent. Having you and your team complete a full K.Y.C will bring trust to the community and will set a certain mindframe for future actions and management of the project. Our partners at K.C Consulting provide the best legal K.Y.C services!"
-            icon={<FiUserCheck size={80} className="text-violet" />}
+            icon={<RiShieldUserLine size={35} />}
           />
           <ServiceCard
             title="Marketing"
             description="Marketing is the best way to reach the masses and potential customers. Our partners at Cardinal House have great resources to help you market your project. Reach thousands of interested people through digital marketing, YouTube reviews, AMAs, community events, and more!"
-            icon={<FiTarget size={80} className="text-violet" />}
+            icon={<FiTarget size={35} />}
           />
           <ServiceCard
             title="Community Events"
             description="A strong project has a strong community. We understand the importance of a well informed, active, and proactive communities. Our partners at Cardinal House provide the best community event management for DeFi projects. Build your community through game nights, giveaways, challenges, surprises, and more!"
-            icon={<MdEmojiEvents size={80} className="text-violet" />}
+            icon={<MdEmojiEvents size={35} />}
           />
           <ServiceCard
-            title="AMAs (Ask Me Anything)"
+            title="AMAs"
             description="AMAs are a great way to reach new potential investors and get fresh eyes on your project. They serve as a very useful tool in sharing what your project is about and who the team is around the project. Contact Cardinal House to schedule an AMA!"
-            icon={<IoMicSharp size={80} className="text-violet" />}
+            icon={<IoMicSharp size={35} />}
           />
           <ServiceCard
             title="Ivy Nest (Coming Soon!)"
             description="You wont want to be late to this once its out!"
-            icon={<GiNestBirds size={80} className="text-violet" />}
+            icon={<GiNestBirds size={35} />}
           />
         </div>
 
