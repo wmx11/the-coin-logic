72e498b0eb4d9f168dbccfec96bedb86271cefcb
Vainius <vmykolaitis1@gmail.com>
2022-09-12 13:17:56 +0300

    Added charts handling to StatsTab

--- apps/client/components/StatTab/StatTab.tsx
+++ apps/client/components/StatTab/StatTab.tsx
@@ -1,6 +1,9 @@
-import { Paper, Popover, Text } from '@mantine/core';
-import React, { FC, useState } from 'react';
+import { Paper, Popover, Text, Tooltip } from '@mantine/core';
+import { FC, useState } from 'react';
 import { FaInfoCircle } from 'react-icons/fa';
+import { HiChartSquareBar } from 'react-icons/hi';
+import { MdCompare } from 'react-icons/md';
+import useChartStore from 'store/useChartStore';
 import { PreviousValueTypes } from '../../types/MarketData';
 import toCurrency from '../../utils/toCurrency';
 import toLocaleString from '../../utils/toLocaleString';
@@ -11,24 +14,54 @@ type StatTabProps = {
   value?: number;
   isCurrency?: boolean;
   tooltip?: string;
+  chartEntry?: string;
+  slug?: string;
+  section?: '' | 'marketData' | 'holdersData' | 'socialMediaData';
   previousValue: PreviousValueTypes;
 };
 
-const StatTab: FC<StatTabProps> = ({ title, value, previousValue, isCurrency, tooltip }) => {
+const StatTab: FC<StatTabProps> = ({ title, value, previousValue, isCurrency, tooltip, chartEntry, slug, section }) => {
   const [opened, setOpened] = useState(false);
+  const chartStore = useChartStore((state) => state);
+
+  const getChartEntryData = async (entry: string) => {
+    const methods = await import('data/getters/charts');
+    const data = await methods[entry as keyof typeof methods](slug as string);
+    return data;
+  };
+
+  const handleChartEntry = async (entry: string) => {
+    chartStore.setLoading(true);
+    const data = await getChartEntryData(entry);
+    chartStore.setLoading(false);
+    chartStore.setChartSection(section as string);
+    chartStore.setChartTitle(title as string);
+    chartStore.setChartData(data.marketStats);
+    chartStore.setNetwork(data.project.network.slug);
+    chartStore.setPairAddress(data.project.pairAddress);
+  };
+
+  const handleCompareChartEntry = async (entry: string) => {
+    chartStore.setLoading(true);
+    const data = await getChartEntryData(entry);
+    chartStore.setLoading(false);
+    chartStore.setCompareChartTitle(title as string);
+    chartStore.setCompareChartData(data.marketStats);
+  };
+
   return (
     <Paper
       p="md"
       shadow="sm"
       withBorder
-      className="w-full min-h-[115px] md:w-[265px] text-center relative hover:shadow-lg transition-shadow"
+      className="w-full min-h-[120px] md:w-[265px] text-center relative hover:shadow-lg transition-shadow"
     >
       <Text className="text-slate-500 font-semibold text-sm">{title}</Text>
       <Text weight={700} className="text-2xl mb-1">
         {isCurrency ? toCurrency(value) : toLocaleString(value)}
       </Text>
       <Trend previousValue={previousValue} />
-      <div className="absolute bottom-1 left-2">
+      <div className="absolute bottom-1 left-0 right-0 px-2 flex justify-between items-center w-full">
         <div className="cursor-pointer">
           <Popover
             opened={opened}
@@ -40,11 +73,35 @@ const StatTab: FC<StatTabProps> = ({ title, value, previousValue, isCurrency, to
             shadow="md"
             target={<FaInfoCircle onClick={() => setOpened(true)} className="text-violet" />}
           >
-            <Text size="sm" pt='xs'>
+            <Text size="sm" pt="xs">
               {tooltip}
             </Text>
           </Popover>
         </div>
+        {chartEntry && (
+          <div className="cursor-pointer flex gap-4 md:gap-2 items-center">
+            <Tooltip
+              label={`View ${title} chart`}
+              withArrow
+              transition="pop"
+              color="violet"
+              wrapLines={true}
+            >
+              <HiChartSquareBar className="text-violet" onClick={() => handleChartEntry(chartEntry)} size={20} />
+            </Tooltip>
+            {chartStore.chartData.length > 0 && chartStore.chartTitle !== title && (
+              <Tooltip
+                label={`Compare ${chartStore.chartTitle} with ${title} chart`}
+                withArrow
+                transition="pop"
+                color="violet"
+                wrapLines={true}
+              >
+                <MdCompare className="text-violet" onClick={() => handleCompareChartEntry(chartEntry)} size={18} />
+              </Tooltip>
+            )}
+          </div>
+        )}
       </div>
     </Paper>
   );
