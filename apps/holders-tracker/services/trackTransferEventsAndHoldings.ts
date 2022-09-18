4134563ef7e25e6772dc5d218065b8a14695bda9
Vainius <vmykolaitis1@gmail.com>
2022-09-18 21:33:56 +0300

    added isRunning check to trackTransferEventsAndHoldings

--- apps/holders-tracker/services/trackTransferEventsAndHoldings.ts
+++ apps/holders-tracker/services/trackTransferEventsAndHoldings.ts
@@ -4,10 +4,19 @@ import trackTransferEvents from './trackTransferEvents';
 /**
  * @param initial - Whether this call is initial (first time for new projects) or a regular call (tracking starts from a previous block)
  */
+let isRunning = false;
 
 const trackTransferEventsAndHoldings = async (initial?: boolean, reset?: boolean) => {
+  if (isRunning) {
+    return null;
+  }
+
+  isRunning = true;
+
   await trackTransferEvents(initial);
   await trackHoldings(initial, reset);
+
+  isRunning = false
 };
 
 export default trackTransferEventsAndHoldings;
