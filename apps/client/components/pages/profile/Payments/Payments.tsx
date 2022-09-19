d2a122b97d533cf9350230770b7832060c971503
Vainius <vmykolaitis1@gmail.com>
2022-09-19 21:36:33 +0300

    Refactored MyProjects and Payments tables

--- apps/client/components/pages/profile/Payments/Payments.tsx
+++ apps/client/components/pages/profile/Payments/Payments.tsx
@@ -1,8 +1,9 @@
-import { Popover, Space, Text } from '@mantine/core';
+import { Space, Text } from '@mantine/core';
+import { Column } from '@table-library/react-table-library/types/compact';
 import GoBack from 'components/GoBack';
 import PaymentStatus from 'components/PaymentStatus';
-import ResponsiveTable from 'components/ResponsiveTable';
-import { useState } from 'react';
+import Table from 'components/Table';
+import { responsiveStylesForLayoutWithSideMenu } from 'components/Table/mainTheme';
 import { Payment } from 'types';
 import { formatDate } from 'utils/formatters';
 import toCurrency from 'utils/toCurrency';
@@ -21,99 +22,49 @@ const Payments = ({ payments }: PaymentsProps) => {
     );
   }
 
-  const [state, setState] = useState(payments.map((x, index) => ({ row: index, isOpen: false })));
+  const theme = {
+    Table: `
+    --data-table-library_grid-template-columns: 95px repeat(2, 1fr) 145px repeat(5, 1fr);
+    ${responsiveStylesForLayoutWithSideMenu}`,
+    BaseCell: `          
+    &:nth-of-type(2) {
+      left: 0;
+    }`,
+    Cell: `   
+    &:nth-of-type(4) > div {
+      white-space: unset;
+      text-overflow: unset;
+    }`,
+  };
 
-  const closeToggle = (index: number) =>
-    setState((prevState) => {
-      prevState[index].isOpen = false;
-      return [...prevState];
-    });
-
-  const openToggle = (index: number) =>
-    setState((prevState) => {
-      if (state[index].row === index) {
-        prevState[index].isOpen = !prevState[index].isOpen;
-      }
-      return [...prevState];
-    });
+  const columns: Column[] = [
+    { label: 'Issued', renderCell: ({ dateIssued }) => formatDate(new Date(dateIssued)) },
+    {
+      label: 'Name',
+      pinLeft: true,
+      renderCell: ({ invoiceUrl, name }) => (
+        <a href={invoiceUrl as string} target="_blank" className="hover:underline">
+          {name}
+        </a>
+      ),
+    },
+    {
+      label: 'Status',
+      renderCell: ({ status }) => <PaymentStatus status={status as string} />,
+    },
+    { label: 'Description', renderCell: ({ description }) => description },
+    { label: 'Qnty', renderCell: ({ quantity }) => quantity },
+    { label: 'Unit price', renderCell: ({ price }) => toCurrency(price as number) },
+    { label: 'Discount', renderCell: ({ discount }) => (discount as number) || '-' },
+    { label: 'Tax', renderCell: ({ tax }) => (tax as number) || '-' },
+    { label: 'Total', renderCell: ({ amount }) => toCurrency(amount as number) },
+  ];
 
   return (
     <div className="w-full">
       <GoBack />
       <Space h="md" />
-      <ResponsiveTable
-        data={{
-          head: [
-            { name: 'Issued', width: 75 },
-            { name: 'Name', width: 80 },
-            { name: 'Status', width: 70 },
-            { name: 'Description', width: 190 },
-            { name: 'Qnty', width: 50 },
-            { name: 'Unit price', width: 80 },
-            { name: 'Discount', width: 70 },
-            { name: 'Tax', width: 50 },
-            { name: 'Total', width: 80 },
-            { name: '...', width: 20 },
-          ],
-          rows: payments.map(
-            (
-              {
-                amount,
-                dateIssued,
-                datePaid,
-                description,
-                discount,
-                invoiceUrl,
-                name,
-                paymentAddress,
-                paymentMethod,
-                price,
-                quantity,
-                status,
-                tax,
-              },
-              index,
-            ) => ({
-              row: [
-                { value: formatDate(new Date(dateIssued)) as string },
-                {
-                  value: (
-                    <a href={invoiceUrl as string} target="_blank" className="hover:underline">
-                      {name}
-                    </a>
-                  ),
-                },
-                { value: <PaymentStatus status={status as string} /> },
-                { value: description as string },
-                { value: quantity as number },
-                { value: toCurrency(price as number) },
-                { value: (discount as number) || '-' },
-                { value: (tax as number) || '-' },
-                { value: toCurrency(amount as number) },
-                {
-                  value: (
-                    <Popover
-                      opened={state[index]?.isOpen}
-                      position="bottom"
-                      spacing="xs"
-                      target={
-                        <div className="cursor-pointer" onClick={() => openToggle(index)}>
-                          ...
-                        </div>
-                      }
-                      onClose={() => closeToggle(index)}
-                    >
-                      <Text component="a" variant="link" href={invoiceUrl as string} target="_blank" size="xs">
-                        View Invoice
-                      </Text>
-                    </Popover>
-                  ),
-                },
-              ],
-            }),
-          ),
-        }}
-      />
+      <Table data={payments} columns={columns} customTheme={theme} />
     </div>
   );
 };
