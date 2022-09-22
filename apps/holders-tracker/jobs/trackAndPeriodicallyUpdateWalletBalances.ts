f9bea2202c17572810c572e576490d847fbf592e
Vainius <vmykolaitis1@gmail.com>
2022-09-22 21:12:10 +0300

    Tweak cron

--- apps/holders-tracker/jobs/trackAndPeriodicallyUpdateWalletBalances.ts
+++ apps/holders-tracker/jobs/trackAndPeriodicallyUpdateWalletBalances.ts
@@ -2,4 +2,4 @@ import cron from 'node-cron';
 import trackAndPeriodicallyUpdateWalletBalances from '../services/trackAndPeriodicallyUpdateWalletBalances';
 
 // Run every 30 minutes to update rebasing token balances
-cron.schedule('*/30 * * * *', trackAndPeriodicallyUpdateWalletBalances);
+cron.schedule('*/30 * * * *', () => trackAndPeriodicallyUpdateWalletBalances());
