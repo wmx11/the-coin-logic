94ba19b401b57e3ee11c12f0928cc31780917176
Vainius <vmykolaitis1@gmail.com>
2022-10-07 17:37:26 +0300

    Tweaked some tings about lowercase address comparison

--- packages/utils/getPrimaryPairAddress.ts
+++ packages/utils/getPrimaryPairAddress.ts
@@ -1,6 +1,8 @@
 import { Project } from 'tcl-packages/types';
 
 const getPrimaryPairAddress = (project: Project) =>
-  project?.liquidityPair?.filter((pair) => pair.isPrimary && pair.order === 1 && pair.address === project.pairAddress);
+  project?.liquidityPair?.filter(
+    (pair) => pair.isPrimary && pair.order === 1 && pair.address.toLowerCase() === project.pairAddress.toLowerCase(),
+  );
 
 export default getPrimaryPairAddress;
