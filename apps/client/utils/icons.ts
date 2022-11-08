89b7aa57833455d6fd366cd63057bf5db035b90c
Vainius <vmykolaitis1@gmail.com>
2022-11-08 11:36:15 +0200

    Added Icons utility

---
+++ apps/client/utils/icons.ts
@@ -0,0 +1,11 @@
+import { FaFileExport, FaPlus, FaRegEdit, FaStop, FaThumbsDown, FaThumbsUp, FaTrash } from 'react-icons/fa';
+
+export const Icons = {
+  Edit: FaRegEdit,
+  Delete: FaTrash,
+  Add: FaPlus,
+  Stop: FaStop,
+  ThumbsUp: FaThumbsUp,
+  ThumbsDown: FaThumbsDown,
+  ExportFile: FaFileExport,
+};
