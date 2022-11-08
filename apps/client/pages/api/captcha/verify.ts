f098fd3bbe6f97ef9df23f838cbf50efd63ade24
Vainius <vmykolaitis1@gmail.com>
2022-11-08 11:33:33 +0200

    Captcha verification now returns an IP address

--- apps/client/pages/api/captcha/verify.ts
+++ apps/client/pages/api/captcha/verify.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
+import { getIpAddress } from 'utils/utils';
 
 const RECAPTHA_SCORE = 0.5;
 
@@ -10,14 +11,18 @@ const verifyRecaptcha = (token: string) => {
   return axios.post(url);
 };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ success: boolean; ip: string | undefined; message: string | undefined }>,
+) {
   const { token } = req.body;
+  const ip = getIpAddress(req);
 
   const recaptchaResponse = await verifyRecaptcha(token);
-
+  
   if (recaptchaResponse.data.success && recaptchaResponse.data.score >= RECAPTHA_SCORE) {
-    return res.status(200).json({ success: true });
+    return res.status(200).json({ success: true, ip, message: '' });
   }
 
-  return res.status(400).json({ success: false });
+  return res.status(400).json({ success: false, ip: undefined, message: '' });
 }
