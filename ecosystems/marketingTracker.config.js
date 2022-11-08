51fd3c4ef592acb176efd43d69004bb1adb0bf01
Vainius <vmykolaitis1@gmail.com>
2022-11-08 11:38:36 +0200

    Added marketing and socials ecosystems

---
+++ ecosystems/marketingTracker.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  apps: [
+    {
+      name: 'marketing-tracker',
+      script: './apps/marketing-tracker/dist/index.js',
+    },
+  ],
+};
