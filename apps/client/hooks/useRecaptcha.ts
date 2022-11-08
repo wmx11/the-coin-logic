5cde7ee8a9c55b8c83228c044557caba2da2ddd6
Vainius <vmykolaitis1@gmail.com>
2022-11-08 11:32:53 +0200

    New hooks

--- apps/client/hooks/useRecaptcha.ts
+++ apps/client/hooks/useRecaptcha.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
 
 const ERROR_MESSAGE = 'Recaptcha has failed. Please try again later.';
 
 const useRecaptcha = () => {
   const [isValid, setIsValid] = useState(false);
+  const [ip, setIp] = useState<string>();
   const [captchaToken, setCaptchaToken] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const { executeRecaptcha } = useGoogleReCaptcha();
@@ -14,7 +15,7 @@ const useRecaptcha = () => {
     if (!executeRecaptcha) {
       setIsValid(false);
       setErrorMessage(ERROR_MESSAGE);
-      return false;
+      return { isValid: false, message: ERROR_MESSAGE, ip: undefined };
     }
 
     const token = await executeRecaptcha();
@@ -22,23 +23,26 @@ const useRecaptcha = () => {
     if (!token) {
       setIsValid(false);
       setErrorMessage(ERROR_MESSAGE);
-      return false;
+      return { isValid: false, message: ERROR_MESSAGE, ip: undefined };
     }
+
     setCaptchaToken(token);
+
     const result = await axios.post('/api/captcha/verify', { token });
 
     if (!result.data.success) {
       setIsValid(false);
       setErrorMessage(ERROR_MESSAGE);
-      return false;
+      return { isValid: false, message: ERROR_MESSAGE, ip: result.data.ip };
     }
 
     setIsValid(true);
+    setIp(result.data.ip);
     setErrorMessage('');
-    return true;
+    return { isValid: true, message: '', ip: result.data.ip };
   };
 
-  return { validate, executeRecaptcha, isValid, errorMessage, captchaToken };
+  return { validate, executeRecaptcha, isValid, errorMessage, captchaToken, ip };
 };
 
 export default useRecaptcha;
--- apps/client/hooks/useUser.ts
+++ apps/client/hooks/useUser.ts
@@ -2,10 +2,11 @@ import { getUserById, getUserProjects, getUserReferrals } from 'data/getters/use
 import { useSession } from 'next-auth/react';
 import { useEffect, useMemo, useState } from 'react';
 import { User } from 'types';
+import { products } from 'types/Products';
 
 const useUser = () => {
   const { data: session, status } = useSession();
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<(User & { ordersCount: number }) | null>(null);
   const userId = session?.id;
 
   const getUser = useMemo(async () => {
@@ -16,14 +17,24 @@ const useUser = () => {
     const user = await getUserById(session.id as string);
     const userReferrals = await getUserReferrals(user.referralCode);
     const projects = await getUserProjects(user.email);
-    return { ...user, ...userReferrals, projects };
+    return { id: userId, ...user, ...userReferrals, projects };
   }, [session]);
+
+  const getSubscription = (): { sku: string; slug: string; name: string } | null => {
+    if (!user?.subscriptionStatus?.isValid) {
+      return null;
+    }
+
+    return user?.subscriptionStatus?.products.filter((item: { sku: string }) =>
+      Object.keys(products.sku).filter((key) => item.sku === products.sku[key as keyof typeof products.sku]),
+    )[0];
+  };
 
   useEffect(() => {
     getUser.then((data) => setUser(data));
   }, [session]);
 
-  return { user, session, status };
+  return { user, session, status, subscription: getSubscription() };
 };
 
 export default useUser;
