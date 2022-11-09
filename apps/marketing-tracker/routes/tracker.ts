e1bdab4cb72af59fef74d0786bcf8676301e33df
Vainius <vmykolaitis1@gmail.com>
2022-11-10 00:10:59 +0200

    Tweaks

--- apps/marketing-tracker/routes/tracker.ts
+++ apps/marketing-tracker/routes/tracker.ts
@@ -2,38 +2,16 @@ import { Router } from 'express';
 import getTrackerResults from '../controllers/getTrackerResults';
 import checkForBots from '../middlewares/checkForBots';
 import { isDev } from '../utils/utils';
-import path from 'path';
 
 const router = Router();
 
 const trackerPath = isDev ? '/:campaignId/:target' : '/:campaignId';
-const index = path.resolve(process.cwd(), 'apps', 'marketing-tracker', 'index.html');
-const css = path.resolve(process.cwd(), 'apps', 'marketing-tracker', 'output.css');
 
 router.get(trackerPath, checkForBots, async (req, res) => {
   try {
-    if (req.subdomains.length > 0) {
-      return getTrackerResults(req, res);
-    }
-    return res.sendFile(index);
+    return getTrackerResults(req, res);
   } catch (error) {
-    return res.sendFile(index);
-  }
-});
-
-router.get('/', checkForBots, async (req, res) => {
-  try {
-    return res.sendFile(index);
-  } catch (error) {
-    return res.sendFile(index);
-  }
-});
-
-router.get('/output.css', checkForBots, async (req, res) => {
-  try {
-    return res.sendFile(css);
-  } catch (error) {
-    return res.sendFile(css);
+    return res.status(500).json({ message: 'There has been an issue handling your request.' });
   }
 });
 
