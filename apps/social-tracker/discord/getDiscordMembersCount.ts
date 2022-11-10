45117541eaa249f23eab5ae8f3592912aa26b262
Vainius <vmykolaitis1@gmail.com>
2022-11-10 12:01:40 +0200

    Errror handling

--- apps/social-tracker/discord/getDiscordMembersCount.ts
+++ apps/social-tracker/discord/getDiscordMembersCount.ts
@@ -1,19 +1,24 @@
 import { Client } from 'discord.js';
 
 export const getDiscordMembersCount = async (client: Client, guildId: string) => {
-  if (!client.isReady()) {
-    console.error('Client is not logged in.');
+  try {
+    if (!client.isReady()) {
+      console.error('Client is not logged in.');
+      return null;
+    }
+
+    const guild = await client.guilds.fetch(guildId);
+
+    if (!guild) {
+      console.error('Guild by the following ID was not found.');
+      return null;
+    }
+
+    const membersCount = guild.approximateMemberCount;
+
+    return membersCount;
+  } catch (error) {
+    console.error(error);
     return null;
   }
-
-  const guild = await client.guilds.fetch(guildId);
-
-  if (!guild) {
-    console.error('Guild by the following ID was not found.');
-    return null;
-  }
-
-  const membersCount = guild.approximateMemberCount;
-
-  return membersCount;
 };
