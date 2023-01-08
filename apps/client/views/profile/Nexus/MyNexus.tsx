93c0b4e6a6822bf0e63803e26330cee1a2f8cf68
Vainius <vmykolaitis1@gmail.com>
2023-01-08 19:33:40 +0200

    My Nexus tweaks

--- apps/client/views/profile/Nexus/MyNexus.tsx
+++ apps/client/views/profile/Nexus/MyNexus.tsx
@@ -17,18 +17,20 @@ const MyNexus: FC<MyNexusProps> = ({ provider }) => {
   return (
     <div className="w-full">
       <GoBack />
-      <div className="my-4">
-        <GradientTitle order={2}>Welcome to your NEXUS profile, {provider.name}</GradientTitle>
-        <Text size="xs" color="dimmed">
-          Here you will find your own NEXUS profile.
-        </Text>
-      </div>
       {provider ? (
-        <div className="grid grid-col-2">
-          <div>
-            <ProviderCard provider={provider} showControls={true} />
+        <>
+          <div className="my-4">
+            <GradientTitle order={2}>Welcome to your NEXUS profile, {provider.name}</GradientTitle>
+            <Text size="xs" color="dimmed">
+              Here you will find your own NEXUS profile.
+            </Text>
           </div>
-        </div>
+          <div className="grid grid-col-2">
+            <div>
+              <ProviderCard provider={provider} showControls={true} />
+            </div>
+          </div>
+        </>
       ) : (
         <GrayBox>
           <div className="text-center">
